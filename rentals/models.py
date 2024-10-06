80983b47b32cf18cf97aa16bcb53620aab1a7d9d
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-06 09:31:51 +0000

    F

--- rentals/models.py
+++ rentals/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Property(models.Model):
-    name = models.CharField(max_length=1255)
-    address = models.CharField(max_length=500)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    name = models.CharField(_("اسم العقار"),max_length=1255)
+    address = models.CharField(_("العنوان"),max_length=500)
+    price = models.DecimalField(_("السعر"),max_digits=10, decimal_places=2)
+    description = models.TextField(_("الوصف"))
 
     def __str__(self):
         return self.name
