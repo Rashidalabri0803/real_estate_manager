54b9355ea49afc9d4400d16ff23cdf3d9e4e5faf
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-06 08:45:51 +0000

    L

--- rentals/urls.py
+++ rentals/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.property_list, name='property_list'),
-    #path('<int:pk>/', views.property_detail, name='property_detail'),
+    path('<int:pk>/', views.property_detail, name='property_detail'),
 ]
