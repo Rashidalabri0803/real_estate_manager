2a85930c079428b960dbca3cc9b19faa8de5f3d4
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-06 08:43:11 +0000

    S

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Property, Tenant, Lease
 # Create your views here.
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, 'property_list.html', {'properties': properties})
+
+def property_detail(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+    return render(request, 'property_detail.html', {'property': property})
