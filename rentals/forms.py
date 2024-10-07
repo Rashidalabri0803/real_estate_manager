ebaaf93bc75fcc865bf1c41ce0f41cd78bc8cc0f
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-07 19:38:58 +0400

    Delete rentals directory

--- rentals/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Rental
-
-class RentalForm(forms.ModelForm):
-    class Meta:
-        model = Rental
-        fields = ['property', 'tenant_name', 'start_date', 'end_date', 'monthly_rent']
