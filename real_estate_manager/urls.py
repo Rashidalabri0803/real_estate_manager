0a9d4e3d4341ecd0d071f5c4516bbcfc5d7a9e0a
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-07 19:36:46 +0400

    Update urls.py

--- real_estate_manager/urls.py
+++ real_estate_manager/urls.py
@@ -16,14 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from . import views
+
 urlpatterns = [
-    #path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('properties/', include('properties.urls')),
-    path('rentals/', include('rentals.urls')),
-    path('payments/', include('payments.urls')),
 ] 
