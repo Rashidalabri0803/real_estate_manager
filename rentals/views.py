be66e71dfd086fecaf08fdcb44e7cea9399d4476
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-07 14:49:43 +0000

    D

--- rentals/views.py
+++ rentals/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Rental
+from .forms import RentalForm
 # Create your views here.
 def rental_list(request):
     rentals = Rental.objects.all()
     return render(request, 'rentals/rental_list.html', {'rentals': rentals})
+
+def rental_detail(request, pk):
+    rental = get_object_or_404(Rental, pk=pk)
+    return render(request, 'rentals/rental_detail.html', {'rental': rental})
+
+def rental_create(request):
+    if request.method == 'POST':
+        form = RentalForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rental_list')
+    else:
+        form = RentalForm()
+    return render(request, 'rentals/rental_form.html', {'form': form})
+
+def rental_update(request, pk):
+    rental = get_object_or_404(Rental, pk=pk)
+    if request.method == 'POST':
+        form = RentalForm(request.POST, instance=rental)
+        if form.is_valid():
+            form.save()
+            return redirect('rental_detail', pk=pk)
+    else:
+        form = RentalForm(instance=rental)
+    return render(request, 'rentals/rental_form.html', {'form': form})
+
+def rental_delete(request, pk):
+    rental = get_object_or_404(Rental, pk=pk)
+    if request.method == 'POST':
+        rental.delete()
+        return redirect('rental_list')
+    return render(request, 'rentals/rental_confirm_delete.html', {'rental': rent
