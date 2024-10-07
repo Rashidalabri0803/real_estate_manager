b343392e9dba1dae47959e15e1342b596e0b383c
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-07 13:30:53 +0000

    Accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenicationForm
+from .forms import RegisterForm
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+      form = AuthenticationForm(request, data=request.POST)
+      if form.is_valid():
+          user = form.get_user()
+          login(request, user)
+          return redirect('dashboard')
+    else:
+      form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form}
+
+def reigster_view(request):
+    if request.method == 'POST':
+      form = RegisterForm(request.POST)
+      if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('dashboard')
+    else:
+      form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
