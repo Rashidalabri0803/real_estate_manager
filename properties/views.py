c388a01170daa6e9c8e9799e90d8feab41408172
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-07 11:15:40 +0400

    s

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Property
 # Create your views here.
+def index(request):
+    properties = Property.objects.all()
+    
--- real_estate_manager/urls.py
+++ real_estate_manager/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rentals.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
