c4c7a989bf87d98e43c82bbfaba3b76a1c9efab2
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-08 15:44:48 +0000

    Admin.py

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Property
 # Register your models here.
-
-admin.site.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'price', 'property_type', 'status', 'created_at')
+    list_filter = ('status', 'property_type', 'location')
+    search_fields = ('name', 'location')
+    list_editable = ('status',)
+    ordering = ('-created_at',)
+  
+admin.site.register(Property, PropertyAdmin)
--- tenants/admin.py
+++ tenants/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Tenant
 # Register your models here.
+
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'id_number', 'address', 'profession', 'registration_date')
+    list_filter = ('registration_date',)
+    search_fields = ('name', 'email', 'phone', 'id_number')
+    ordering = ('-registration_date',)
+
+admin.site.register(Tenant, TenantAdmin)
