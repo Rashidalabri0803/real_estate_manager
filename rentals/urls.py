4995d557d1683fe4b70596948726ba053ca335e3
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-09 20:06:54 +0400

    Update urls.py

--- rentals/urls.py
+++ rentals/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.rental_list, name='rental_list'),
-    path('rental/<int:pk>/', views.rental_detail, name='rental_detail'),
-    path('rental/create/', views.rental_create, name='rental_create'),
-    path('rental/<int:pk>/edit/', views.rental_update, name='rental_update'),
-    path('rental/<int:pk>/delete/', views.rental_delete, name='rental_delete')
+    path('<int:pk>/', views.rental_detail, name='rental_detail'),
+    path('create/', views.rental_create, name='rental_create'),
+    path('<int:pk>/edit/', views.rental_update, name='rental_update'),
+    path('<int:pk>/delete/', views.rental_delete, name='rental_delete'),
 ]
